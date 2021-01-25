722432be05fa4801d16f71c9549ca42a246a1f1b
MBit2021 <77882685+MBit2021@users.noreply.github.com>
2021-01-26 00:16:30 +0100

    Added turn command

--- main.ts
+++ main.ts
@@ -2,8 +2,9 @@ function InitDriveProfile () {
     drive_mode = 0
     drive_gear = 0
     drive_status = 0
-    drive_turntime = 200
+    drive_rotatetime = 300
     gear_shifttime = 300
+    rotate_speed = [180, 180]
     gear_speed = [80, 150, 250, 100]
     sonar_alarm = 50
     sonar_warn = 100
@@ -34,8 +35,16 @@ function StopDrive () {
     drive_gear += 0
     drive_status += 0
 }
-function TurnToLeft () {
-	
+function DriveTurn (rturn: number, keepmove: number) {
+    if (rturn == 1) {
+        OSOYOO_Robot.CarCtrlSpeed(OSOYOO_Robot.CarState.Car_SpinRight, rotate_speed[0])
+    } else {
+        OSOYOO_Robot.CarCtrlSpeed(OSOYOO_Robot.CarState.Car_SpinLeft, rotate_speed[0])
+    }
+    basic.pause(drive_rotatetime)
+    if (true) {
+    	
+    }
 }
 function EnableRemoteIR (on: number) {
     if (on != 0) {
@@ -56,15 +65,14 @@ function DriveForward () {
     if (drive_status == 4) {
         StopDrive()
     }
+    if (drive_gear == 0) {
+        drive_gear += 1
+    }
     if (drive_gear < 3) {
         OSOYOO_Robot.CarCtrlSpeed(OSOYOO_Robot.CarState.Car_Run, gear_speed[drive_gear - 1])
         drive_gear += drive_gear + 1
         drive_status += 1
-        basic.pause(gear_shifttime)
     }
-}
-function TurnToRight () {
-	
 }
 function ManageDrive (tgtsts: number) {
 	
@@ -76,15 +84,21 @@ OSOYOO_IR_BLACK.onPressEvent(RemoteButton.NUM1, function () {
     drive_mode += 1
 })
 function DriveReverse () {
-	
+    if (drive_status == 1) {
+        StopDrive()
+    }
+    drive_gear += 4
+    OSOYOO_Robot.CarCtrlSpeed(OSOYOO_Robot.CarState.Car_Run, gear_speed[drive_gear - 1])
+    drive_status += 4
 }
 let system_idle_time = 0
 let sonar_enable = 0
 let sonar_warn = 0
 let sonar_alarm = 0
 let gear_speed: number[] = []
+let rotate_speed: number[] = []
 let gear_shifttime = 0
-let drive_turntime = 0
+let drive_rotatetime = 0
 let drive_status = 0
 let drive_gear = 0
 let drive_mode = 0
