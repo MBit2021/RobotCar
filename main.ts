e73e9dfd912dbe53f210acb5fdd26873968dcee6
MBit2021 <77882685+MBit2021@users.noreply.github.com>
2021-01-25 09:58:08 +0100

    Refactored drive forward command

--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
 function InitDriveProfile () {
     drive_mode = 0
-    drive_gear = 1
+    drive_gear = 0
     drive_status = 0
     drive_turntime = 200
-    gear_shirttime = 300
+    gear_shifttime = 300
+    gear_speed = [80, 150, 250, 100]
     sonar_alarm = 50
     sonar_warn = 100
     sonar_enable = 0
     system_idle_time = 100
-    gear_speed = [80, 150, 250, 100]
 }
 function EnableBeam (on: number) {
     if (on != 0) {
@@ -30,7 +30,9 @@ bluetooth.onBluetoothDisconnected(function () {
     basic.clearScreen()
 })
 function StopDrive () {
-	
+    OSOYOO_Robot.CarCtrl(OSOYOO_Robot.CarState.Car_Stop)
+    drive_gear += 0
+    drive_status += 0
 }
 function TurnToLeft () {
 	
@@ -51,10 +53,14 @@ function EnableRemoteIR (on: number) {
     }
 }
 function DriveForward () {
+    if (drive_status == 4) {
+        StopDrive()
+    }
     if (drive_gear < 3) {
+        OSOYOO_Robot.CarCtrlSpeed(OSOYOO_Robot.CarState.Car_Run, gear_speed[drive_gear - 1])
         drive_gear += drive_gear + 1
-        OSOYOO_Robot.CarCtrlSpeed(OSOYOO_Robot.CarState.Car_Run, gear_speed[drive_gear])
-        basic.pause(gear_shirttime)
+        drive_status += 1
+        basic.pause(gear_shifttime)
     }
 }
 function TurnToRight () {
@@ -72,12 +78,12 @@ OSOYOO_IR_BLACK.onPressEvent(RemoteButton.NUM1, function () {
 function DriveReverse () {
 	
 }
-let gear_speed: number[] = []
 let system_idle_time = 0
 let sonar_enable = 0
 let sonar_warn = 0
 let sonar_alarm = 0
-let gear_shirttime = 0
+let gear_speed: number[] = []
+let gear_shifttime = 0
 let drive_turntime = 0
 let drive_status = 0
 let drive_gear = 0
