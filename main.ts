d950c41532a2bb8efd9e8a9ab294c46d3efe8849
MBit2021 <77882685+MBit2021@users.noreply.github.com>
2021-01-26 11:26:40 +0100

    Refactored rotate command

--- main.ts
+++ main.ts
@@ -2,10 +2,10 @@ function InitDriveProfile () {
     drive_mode = 0
     drive_gear = 0
     drive_status = 0
-    drive_rotatetime = 300
+    rotate_time = 300
     gear_shifttime = 300
     rotate_speed = [180, 180]
-    gear_speed = [80, 150, 250, 100]
+    gear_speed = [100, 200, 300, 100]
     sonar_alarm = 10
     sonar_warn = 20
     sonar_enable = 0
@@ -38,33 +38,14 @@ function StopDrive () {
     drive_gear += 0
     drive_status += 0
 }
-function DriveTurn (rturn: number, keepmove: number) {
-    if (rturn == 1) {
-        OSOYOO_Robot.CarCtrlSpeed(OSOYOO_Robot.CarState.Car_SpinRight, rotate_speed[0])
-    } else {
-        OSOYOO_Robot.CarCtrlSpeed(OSOYOO_Robot.CarState.Car_SpinLeft, rotate_speed[0])
-    }
-    basic.pause(drive_rotatetime)
-    if (keepmove == 1) {
-        if (drive_status == 1) {
-            DriveForward()
-        } else if (drive_status == 4) {
-            DriveReverse()
-        } else {
-            StopDrive()
-        }
-    } else {
-        StopDrive()
-    }
-}
 OSOYOO_IR_BLACK.onPressEvent(RemoteButton.UP, function () {
     DriveForward()
 })
 OSOYOO_IR_BLACK.onPressEvent(RemoteButton.RIGHT, function () {
-    DriveTurn(1, 1)
+    DriveRotate(1, 1)
 })
 function EnableRemoteIR (on: number) {
-    if (on != 0) {
+    if (on == 1) {
         OSOYOO_IR_BLACK.init(Pins.P8)
         basic.showLeds(`
             # # # # #
@@ -98,11 +79,35 @@ OSOYOO_IR_BLACK.onPressEvent(RemoteButton.NUM0, function () {
     drive_mode += 0
 })
 OSOYOO_IR_BLACK.onPressEvent(RemoteButton.LEFT, function () {
-    DriveTurn(0, 1)
+    DriveRotate(0, 1)
 })
 OSOYOO_IR_BLACK.onPressEvent(RemoteButton.DOWN, function () {
     DriveReverse()
 })
+function DriveRotate (toright: number, keepmove: number) {
+    if (toright == 1) {
+        OSOYOO_Robot.CarCtrlSpeed(OSOYOO_Robot.CarState.Car_SpinRight, rotate_speed[0])
+    } else {
+        OSOYOO_Robot.CarCtrlSpeed(OSOYOO_Robot.CarState.Car_SpinLeft, rotate_speed[0])
+    }
+    basic.pause(rotate_time)
+    if (keepmove == 1) {
+        if (drive_status == 1) {
+            DriveForward()
+        } else if (drive_status == 4) {
+            DriveReverse()
+        } else {
+            StopDrive()
+        }
+    } else {
+        StopDrive()
+        if (toright == 0) {
+            drive_status += 2
+        } else {
+            drive_status += 3
+        }
+    }
+}
 OSOYOO_IR_BLACK.onPressEvent(RemoteButton.NUM1, function () {
     drive_mode += 1
 })
@@ -121,7 +126,7 @@ let sonar_alarm = 0
 let gear_speed: number[] = []
 let rotate_speed: number[] = []
 let gear_shifttime = 0
-let drive_rotatetime = 0
+let rotate_time = 0
 let drive_status = 0
 let drive_gear = 0
 let drive_mode = 0
